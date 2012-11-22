1d208c2796e9ff7f549f06154b407a545cc3d037
Daniel Aguilar <dacmail@gmail.com>
2012-11-22 18:13:05 +0100

    Optimización de los metas de head

--- header.php
+++ header.php
@@ -1,19 +1,15 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="content-Type" content="text/html; charset=utf-8" />
 	<title> <?php bloginfo('name'); ?> <?php wp_title(); ?></title>
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" charset="utf-8" />
-	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
-	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
-	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" media="all" charset="utf-8" />
 	<link rel="shortcut icon" href="<?php bloginfo('template_directory')?>/favicon.ico" />
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-	<script type="text/javascript" charset="utf-8" src="<?php bloginfo('template_directory')?>/js/default.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+	<script charset="utf-8" src="<?php bloginfo('template_directory')?>/js/default.js"></script>
 	<!--[if lt IE 9]>
         <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php wp_head(); ?>
 </head>
 <body>
